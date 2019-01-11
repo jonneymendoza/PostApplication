3aa52ffcf33450ff90dc724eb597e72d19545700
Jonathan Richards <jrichards1408@gmail.com>
2019-01-11 23:01:47 +0000

    updated koin test lib

---
+++ app/src/test/java/com/richards/jonathan/postapp/MyTest.kt
@@ -0,0 +1,35 @@
+package com.richards.jonathan.postapp
+
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
+import org.junit.Test
+import org.koin.dsl.module.module
+import org.koin.standalone.StandAloneContext.startKoin
+import org.koin.standalone.get
+import org.koin.standalone.inject
+import org.koin.test.KoinTest
+
+class MyTest : KoinTest {
+
+    class ComponentA
+    class ComponentB(val a: ComponentA)
+
+    val appModule = module {
+        single { ComponentA() }
+        single { ComponentB(get()) }
+    }
+
+    // Lazy inject property
+    val componentB : ComponentB by inject()
+
+    @Test
+    fun `should inject my components`() {
+        startKoin(listOf(appModule))
+
+        // directly request an instance
+        val componentA = get<ComponentA>()
+
+        assertNotNull(componentA)
+        assertEquals(componentA, componentB.a)
+    }
+}
